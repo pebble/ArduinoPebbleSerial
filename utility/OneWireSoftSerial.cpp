f879dbe0974e13709811245d50598ea13baefc55
bgomberg <bgomberg@getpebble.com>
2015-08-20 16:55:04 -0700

    Added license

--- utility/OneWireSoftSerial.cpp
+++ utility/OneWireSoftSerial.cpp
@@ -2,8 +2,18 @@
  * This file provides a one-wire (aka half duplex) software serial implementation to faciliate easy
  * connection and communication with a Pebble watch via the smartstrap port. It was heavily inspired
  * by Arduino's software serial library, with the code for calculating the delays copied verbatim.
- * As is required by the license on Arduino's software serial library, this library is released
+ * As is required by the license on Arduino's software serial library, this file is released
  * under the LGPL v2.1 license.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  */
 
 #include "OneWireSoftSerial.h"
